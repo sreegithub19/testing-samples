72cff8a620ae8a143b1a70b00ea08eff133f501c
sreegithub19 <cpadmaja2003@gmail.com>
2025-06-14 12:26:31 +0530

    C

--- ui/PreviewScreenshot/app/build.gradle.kts
+++ ui/PreviewScreenshot/app/build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -15,9 +31,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        // Screenshot test runner (optional but recommended for screenshot workflows)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -32,31 +46,21 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
     buildFeatures {
         compose = true
     }
-
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.13"
     }
 
-    // Enable Screenshot Testing
     experimentalProperties["android.experimental.enableScreenshotTest"] = true
-
-    // Optional: Screenshot Testing Configuration
-    screenshotTesting {
-        recordMode.set(true) // Set to true to update reference screenshots
-    }
 
     packaging {
         resources {
@@ -74,17 +78,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-
     testImplementation(libs.junit)
-
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-
-    // Screenshot testing dependency
-    androidTestImplementation("androidx.compose.ui:ui-test-screenshot:1.6.0-beta01")
-
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
