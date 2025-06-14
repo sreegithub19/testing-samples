f2352cd6eb9b339744f35e9fe705e30a18a364ee
sreegithub19 <cpadmaja2003@gmail.com>
2025-06-14 13:57:40 +0530

    C

--- ui/PreviewScreenshot/app/src/main/java/com/example/compose/previewscreenshot/MainActivity.kt
+++ ui/PreviewScreenshot/app/src/main/java/com/example/compose/previewscreenshot/MainActivity.kt
@@ -1,35 +1,18 @@
-/*
- * Copyright (C) 2024 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.                            
- */
-
 package com.example.compose.previewscreenshot
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.compose.previewscreenshot.ui.theme.SampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,9 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HtmlPage(modifier = Modifier.fillMaxSize())
                 }
             }
         }
@@ -49,21 +30,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Surface {
-        Text(
-            text = stringResource(id = R.string.greeting),
-            modifier = modifier
-        )
-    }
+fun HtmlPage(modifier: Modifier = Modifier) {
+    val htmlContent = stringResource(id = R.string.greeting_html)
+
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                loadDataWithBaseURL(null, htmlContent, "text/html", "UTF-8", null)
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Preview(showBackground = true, locale = "es")
-@Preview(showBackground = true, name = "Dark" , uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun GreetingPreview() {
+fun HtmlPagePreview() {
     SampleTheme {
-        Greeting()
+        HtmlPage()
     }
 }
